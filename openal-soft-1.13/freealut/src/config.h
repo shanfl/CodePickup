d59ee8e6a30a720309f6c5294a2a1d32daf12f39
sfl <lyricc@sina.cn>
2012-02-16 14:30:25 +0800

    add openal

---
+++ openal-soft-1.13/freealut/src/config.h
@@ -0,0 +1,20 @@
+#define ALUT_BUILD_LIBRARY
+
+#define _XOPEN_SOURCE 500
+
+#define _POSIX_C_SOURCE 199309
+
+#define __NO_CTYPE 1
+
+#define HAVE_WINDOWS_H 1
+
+#define HAVE_SLEEP 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_BASETSD_H 1
+
+#define HAVE__STAT 1
+
+#define HAVE___ATTRIBUTE__ 1
+
